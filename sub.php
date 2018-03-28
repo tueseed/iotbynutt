946f03242c8cab69b78647177940f8b665fbf231
tueseed <tue_seed@hotmail.com>
2018-03-28 22:16:36 +0700

    Update sub.php

--- sub.php
+++ sub.php
@@ -6,22 +6,22 @@ $topic = '/ESP/LED';
 $username = "test";                   
 $password = "12345"; 
 $client_id = "phpMQTT-web";
-
+if(!$mqtt->connect(true, NULL, $username, $password)) {
+	exit(1);
+}
 
     
 $mqtt = new Bluerhinos\phpMQTT('m12.cloudmqtt.com', '19053', $client_id);
-if ($mqtt->connect(true, NULL, $username, $password)) {
-	
-  $topics['/ESP/LED'] = array(
-      "qos" => 0,
-      "function" => "procmsg"
-  );
-  $mqtt->subscribe($topics,0);
-  while($mqtt->proc()) {}
-  $mqtt->close();
-} else {
-  exit(1);
+$topics[$topic] = array("qos" => 0, "function" => "procmsg");
+$mqtt->subscribe($topics, 0);
+while($mqtt->proc()){
+		
 }
+$mqtt->close();
+
+
+
+
 function procmsg($topic, $msg){
   echo "Msg Recieved: $msg\n";
 }
