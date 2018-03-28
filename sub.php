5a4b7724a96c0d43d70ae15f4159a91dad2c290a
tueseed <tue_seed@hotmail.com>
2018-03-28 22:40:16 +0700

    Update sub.php

--- sub.php
+++ sub.php
@@ -6,11 +6,12 @@ $port = 19053;                     // change if necessary
 $username = "test";                   // set your username
 $password = "12345";                   // set your password
 $client_id = "phpMQTT-subscriberweb"; // make sure this is unique for connecting to sever - you could use uniqid()
+$qq = '/ESP/LED';
 $mqtt = new Bluerhinos\phpMQTT($server, $port, $client_id);
 if(!$mqtt->connect(true, NULL, $username, $password)) {
 	exit(1);
 }
-$topics["/ESP/LED"] = array("qos" => 0, "function" => "procmsg");
+$topics[$qq] = array("qos" => 0, "function" => "procmsg");
 $mqtt->subscribe($topics, 0);
 while($mqtt->proc()){
 		
