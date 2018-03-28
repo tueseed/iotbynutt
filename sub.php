b4648269fb3665f8e3d910377487942fe4533cb5
tueseed <tue_seed@hotmail.com>
2018-03-28 21:36:35 +0700

    Add files via upload

---
+++ sub.php
@@ -0,0 +1,26 @@
+<?php
+require_once('phpMQTT.php');
+
+$url = parse_url(getenv('m12.cloudmqtt.com'));
+$topic = substr($url['/ESP/LED'], 1);
+$username = "test";                   
+$password = "12345"; 
+$client_id = "phpMQTT-subscriber";
+
+function procmsg($topic, $msg){
+  echo "Msg Recieved: $msg\n";
+}
+    
+$mqtt = new Bluerhinos\phpMQTT('m12.cloudmqtt.com', '19053', $client_id);
+if ($mqtt->connect(true, NULL, $username, $password)) {
+	
+  $topics[$topic] = array(
+      "qos" => 0,
+      "function" => "procmsg"
+  );
+  $mqtt->subscribe($topics,0);
+  while($mqtt->proc()) {}
+  $mqtt->close();
+} else {
+  exit(1);
+}
