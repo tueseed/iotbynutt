8c78411dbc6bc1e0caf4a3235498c49e5939ebd8
tueseed <tue_seed@hotmail.com>
2018-03-28 21:52:37 +0700

    Create pub.php

---
+++ pub.php
@@ -0,0 +1,16 @@
+require_once('phpMQTT.php');
+$url = parse_url(getenv('m12.cloudmqtt.com'));
+$topic =  '/ESP/LED';
+$client_id = "phpMQTT-pubweb";
+$username = "test";                   
+$password = "12345"; 
+$message = "ON";
+
+$mqtt = new Bluerhinos\phpMQTT('m12.cloudmqtt.com', '19053', $client_id);
+if ($mqtt->connect(true, NULL, $username, $password)) {
+    $mqtt->publish($topic, $message, 0);
+    echo "Published message: " . $message;
+    $mqtt->close();
+}else{
+    $return_msg = "MQTT FAIL...";	
+}	
