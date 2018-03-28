fbcb46799a816ea7fa270dec493fde948309d91a
tueseed <tue_seed@hotmail.com>
2018-03-28 22:50:49 +0700

    Update sub.php

--- sub.php
+++ sub.php
@@ -5,14 +5,14 @@ $server = "m12.cloudmqtt.com";     // change if necessary
 $port = 19053;                     // change if necessary
 $username = "test";                   // set your username
 $password = "12345";                   // set your password
-$client_id = "phpMQTT-subscriberweb"; // make sure this is unique for connecting to sever - you could use uniqid()
+$client_id = "phpMQTT-pubweb"; // make sure this is unique for connecting to sever - you could use uniqid()
 $qq = '/ESP/LED';
 $mqtt = new Bluerhinos\phpMQTT($server, $port, $client_id);
 if(!$mqtt->connect(true, NULL, $username, $password)) {
 	exit(1);
 }
-$topics[$qq] = array("qos" => 1, "function" => "procmsg");
-$mqtt->subscribe($topics, 1);
+$topics[$qq] = array("qos" => 0, "function" => "procmsg");
+$mqtt->subscribe($topics, 0);
 while($mqtt->proc()){
 		
 }
