2d87c9eaddd72685d6f02f8e6843ee2ab587b3f4
tueseed <tue_seed@hotmail.com>
2018-04-25 10:03:30 +0700

    Add files via upload

--- test.php
+++ test.php
@@ -10,6 +10,12 @@
 require('./connect-db-nkw.php');
 $sql_search = "SELECT * FROM tbl_cs";
 $result = mysqli_query($conn,$sql_search);
+while($objectresult = mysqli_fetch_array($result))
+				{
+					echo $objectresult["cs_name"];
+					//echo "<li><a href='csdetial.php?pea_no=".$objectresult["pea_no"]."'>".$objectresult["cs_name"]."</a></li>";
+					
+					}
 
 
 		 ?>
