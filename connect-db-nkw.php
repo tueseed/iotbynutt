2a927e6666e45b6996522b63eb22db23aa7444fb
tueseed <tue_seed@hotmail.com>
2018-04-25 09:37:53 +0700

    Update connect-db-nkw.php

--- connect-db-nkw.php
+++ connect-db-nkw.php
@@ -14,7 +14,7 @@
     $server = "us-cdbr-iron-east-05.cleardb.net";
     $username = "bde447b744111c";
     $password = "e35fa8d6";
-    $db = "heroku_58997c1013c3e1a";
+    $db = "heroku_58997c1013c3e1";
     $conn = new mysqli($server, $username, $password, $db);
     // Check connection
     if ($conn->connect_error) {
