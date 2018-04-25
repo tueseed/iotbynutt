3e7720d1541bcef0893b77b40e3f2da65f0e8831
tueseed <tue_seed@hotmail.com>
2018-04-25 09:57:17 +0700

    Add files via upload

---
+++ test.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<?php
+require('./connect-db-nkw.php');
+
+
+
+
+		 ?>
+</body>
+</html>
