51efbde4506287ebf906129fdc6d089ca961ce14
tueseed <tue_seed@hotmail.com>
2018-04-25 09:26:42 +0700

    Create connect-db-nkw.php

---
+++ connect-db-nkw.php
@@ -0,0 +1,23 @@
+<?php
+    // $servername = "localhost";
+    // $username = "root";
+    // $password = "";
+    // $db = "voc";
+    // // Create connection
+    // $conn = new mysqli($servername, $username, $password, $db);
+    // // Check connection
+    // if ($conn->connect_error) {
+    //     die("Connection failed: " . $conn->connect_error);
+    // }
+    // mysqli_query($conn, "SET NAMES utf8");
+   // $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $server = "us-cdbr-iron-east-05.cleardb.net";
+    $username = "bde447b744111c";
+    $password = "e35fa8d6";
+    $db = "heroku_58997c1013c3e1a";
+    $conn = new mysqli($server, $username, $password, $db);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    mysqli_query($conn, "SET NAMES utf8");
