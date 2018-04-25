f467587a7d5942eaefe54b6a5462a954d2213ef1
tueseed <tue_seed@hotmail.com>
2018-04-25 10:31:46 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -42,7 +42,7 @@ if($office == "NKW")
 				
 										}
 			else{
-							$sql_search = "SELECT * FROM tbl_cs WHERE cs_name LIKE '%".$keyword."%'";
+							$sql_search = "SELECT * FROM tbl_cs WHERE cs_name LIKE '%".$keyword."%' OR pea_no LIKE '%".$keyword."%'";
 				}
 $result = mysqli_query($conn,$sql_search);
 }
