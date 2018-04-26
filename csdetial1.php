37d11dc94de62e45bf5e5c2fcc91dc4a6db29e74
tueseed <tue_seed@hotmail.com>
2018-04-26 20:08:59 +0700

    Add files via upload

--- csdetial1.php
+++ csdetial1.php
@@ -25,7 +25,7 @@ if($office == "PTM"){
 				
 										}
 			else{
-							$sql_search = "SELECT * FROM tbl_cs WHERE (cs_name LIKE '%".$keyword."%') OR (pea_no LIKE '%".$keyword."%') OR (ca LIKE '%".$keyword."%')";
+							$sql_search = "SELECT * FROM tbl_cs WHERE (cs_name LIKE '%".$keyword."%') OR (pea_no LIKE '%".$keyword."%') OR (ca LIKE '%".$keyword."%') OR (address LIKE '%".$keyword."%')";
 				}
 $result = mysqli_query($conn,$sql_search);
 $find_num = mysqli_num_rows($result);
@@ -43,7 +43,7 @@ if($office == "NKW")
 				
 										}
 			else{
-							$sql_search = "SELECT * FROM tbl_cs WHERE (cs_name LIKE '%".$keyword."%') OR (pea_no LIKE '%".$keyword."%') OR (ca LIKE '%".$keyword."%')";
+							$sql_search = "SELECT * FROM tbl_cs WHERE (cs_name LIKE '%".$keyword."%') OR (pea_no LIKE '%".$keyword."%') OR (ca LIKE '%".$keyword."%') OR (address LIKE '%".$keyword."%')";
 				}
 $result = mysqli_query($conn,$sql_search);
 $find_num = mysqli_num_rows($result);
