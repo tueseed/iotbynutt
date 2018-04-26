6b09ed9147bda81629b326bf93d192b894b4ac07
tueseed <tue_seed@hotmail.com>
2018-04-26 20:23:11 +0700

    Add files via upload

--- test.php
+++ test.php
@@ -1,17 +1,28 @@
-<!doctype html>
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta charset="utf-8">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Untitled Document</title>
 </head>
 
 <body>
 <?php
-	$str1 ="สมคิด บ้านเลือก นะจ๊ะ";
-	$str2 = explode(" ",$str1);
-print_r($str2);
-
-
+	$keyword = $_GET["keyword"];
+	$str2 = explode(" ",$keyword);
+	if($keyword){
+				$sql = "SELECT * FROM tbl_cs WHERE (cs_name LIKE '%".$keyword."%')";
+				foreach($str2 as $k){
+						$sql .= " OR (cs_name LIKE '%".$k."%')";
+						$sql .= " OR (pea_no LIKE '%".$k."%')";
+						$sql .= " OR (ca LIKE '%".$k."%')";
+						$sql .= " OR (address LIKE '%".$k."%')";
+									}
+				echo $sql;
+			}
 		 ?>
+         <form action="index.php" method="get">
+         <input name="keyword" id="keyword" type="text" />
+         <input name="" type="submit" />
+         </form>
 </body>
 </html>
