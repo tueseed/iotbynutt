c09fd7d8b495c0e6a2ca57a0ac0fd02f240ca83b
tueseed <tue_seed@hotmail.com>
2018-04-26 19:45:21 +0700

    Update test.php

--- test.php
+++ test.php
@@ -7,15 +7,7 @@
 
 <body>
 <?php
-require('./connect-db-nkw.php');
-$sql_search = "SELECT * FROM tbl_cs";
-$result = mysqli_query($conn,$sql_search);
-while($objectresult = mysqli_fetch_array($result))
-				{
-					echo $objectresult["cs_name"];
-					//echo "<li><a href='csdetial.php?pea_no=".$objectresult["pea_no"]."'>".$objectresult["cs_name"]."</a></li>";
-					
-					}
+echo "fkdjfmnfkd";
 
 
 		 ?>
