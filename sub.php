e76066afe152edee774bddb9100c845fd6c54599
tueseed <tue_seed@hotmail.com>
2018-12-29 12:16:59 +0700

    Add files via upload

--- sub.php
+++ sub.php
@@ -1,24 +1,21 @@
 <?php
-require_once('phpMQTT.php');
+require('phpMQTT.php');
 //$topic =  '/ESP/ST1';
 $username = "test";                   
 $password = "12345"; 
-$client_id = "phpMQTT-subscriber";
+$client_id = "phpMQTT-subscriber";    
+$mqtt = new Bluerhinos\phpMQTT('m12.cloudmqtt.com', '19053', $client_id);
+if (!$mqtt->connect(true, NULL, $username, $password)) 
+{
+	exit(1);
+} 
+$topics['/ESP/ST1'] = array("qos" => 0,"function" => "procmsg");
+$mqtt->subscribe($topics,0);
+while($mqtt->proc()) {}
+$mqtt->close();
 
 function procmsg($topic, $msg){
-  echo "Msg Recieved: $msg\n";
-}
-    
-$mqtt = new Bluerhinos\phpMQTT('m12.cloudmqtt.com', '19053', $client_id);
-if ($mqtt->connect(true, NULL, $username, $password)) {
-	
-  $topics['/ESP/ST1'] = array(
-      "qos" => 0,
-      "function" => "procmsg"
-  );
-  $mqtt->subscribe($topics,0);
-  while($mqtt->proc()) {}
-  $mqtt->close();
-} else {
-  exit(1);
+		echo "Msg Recieved: " . date("r") . "\n";
+		echo "Topic: {$topic}\n\n";
+		echo "\t$msg\n\n";
 }
