ad627c417dee2b32e0a3a88169214d8a1c1a4718
tueseed <tue_seed@hotmail.com>
2018-12-29 12:36:26 +0700

    Add files via upload

--- sub.php
+++ sub.php
@@ -9,7 +9,7 @@ if (!$mqtt->connect(true, NULL, $username, $password))
 {
 	exit(1);
 } 
-$topics['/ESP/ST1'] = array("qos" => 0,"function" => "procmsg");
+$topics['ESP/ST1'] = array("qos" => 0,"function" => "procmsg");
 $mqtt->subscribe($topics,0);
 while($mqtt->proc()) {}
 $mqtt->close();
