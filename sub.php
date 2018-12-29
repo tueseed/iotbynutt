8119eeba90fd76699a4a4d9288cb164b350a42a5
tueseed <tue_seed@hotmail.com>
2018-12-29 13:28:50 +0700

    Add files via upload

--- sub.php
+++ sub.php
@@ -16,9 +16,9 @@ $mqtt->subscribe($topics,0);
 while($mqtt->proc()) {}
 /*
 $mqtt->close();
-*/
+
 function procmsg($topic, $msg){
 		echo "Msg Recieved: " . date("r") . "\n";
 		echo "Topic: {$topic}\n\n";
 		echo "\t$msg\n\n";
-}
+}/*
