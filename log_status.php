f8dc16beb0ad82f7542cc8b43761a6f21d3bcf67
tueseed <tue_seed@hotmail.com>
2018-12-29 15:24:32 +0700

    Add files via upload

--- log_status.php
+++ log_status.php
@@ -6,8 +6,8 @@
 	$data = $_POST["data"];
 	$room = substr($data,0,5);
 	$status = substr($data,5,2);
-	$sql_update_sts = "UPDATE tbl_status SET status='$status' WHERE room='".$room."'";
-	mysqli_query($conn,sql_update_sts);
+	$sql_update_sts = "UPDATE tbl_status SET status='".$status."' WHERE room='".$room."'";
+	mysqli_query($conn,$sql_update_sts);
 	echo "INSERT SUCCESSFULL..".$room."  ".$status;
 	}
 	else
