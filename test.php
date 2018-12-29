2b16285649a8f6205a1687f8954795dd10fd2f3e
tueseed <tue_seed@hotmail.com>
2018-12-29 14:01:51 +0700

    Add files via upload

--- test.php
+++ test.php
@@ -1,38 +1,22 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
-</head>
-
-<body>
-         <form action="test.php" method="get">
-         <input name="keyword" id="keyword" type="text" />
-         <input name="" type="submit" />
-         </form>
 <?php
-require('./connect-db.php');
-	$keyword = $_GET["keyword"];
-	$str2 = explode(" ",$keyword);
-	if($keyword){
-				$sql = "SELECT * FROM tbl_cs WHERE (cs_name LIKE '%".$keyword."%')";
-				foreach($str2 as $k){
-						$sql .= " AND (cs_name LIKE '%".$k."%')";
-						$sql .= " AND (pea_no LIKE '%".$k."%')";
-						$sql .= " AND (ca LIKE '%".$k."%')";
-						$sql .= " AND (address LIKE '%".$k."%')";
-									}
-				$result = mysqli_query($conn,$sql);
-				$a = 1 ;
-				while($objectresult = mysqli_fetch_array($result))
-				{
-					echo $a.".".$objectresult["cs_name"]." ".$objectresult["pea_no"]." ".$objectresult["address"]."<br>";
-					
-					$a = $a+1;
-					}			
-				$a = 0;
-				
-			}
-			?>
-</body>
-</html>
+require("phpMQTT.php");
+$server = "m12.cloudmqtt.com";     // change if necessary
+$port = 19053;                     // change if necessary
+$username = "test";                   // set your username
+$password = "12345";                   // set your password
+$client_id = "phpMQTT-subscriber"; // make sure this is unique for connecting to sever - you could use uniqid()
+$mqtt = new phpMQTT($server, $port, $client_id);
+if(!$mqtt->connect(true, NULL, $username, $password)) {
+	exit(1);
+}
+$topics['bluerhinos/phpMQTT/examples/publishtest'] = array("qos" => 0, "function" => "procmsg");
+$mqtt->subscribe($topics, 0);
+while($mqtt->proc()){
+		
+}
+$mqtt->close();
+function procmsg($topic, $msg){
+		echo "Msg Recieved: " . date("r") . "\n";
+		echo "Topic: {$topic}\n\n";
+		echo "\t$msg\n\n";
+}
