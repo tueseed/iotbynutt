dfdd1858d027f822fcf3da8ff1949e9034b343cd
tueseed <tue_seed@hotmail.com>
2018-12-29 11:43:06 +0700

    Add files via upload

--- control.js
+++ control.js
@@ -1,59 +1,19 @@
-function send_cmd(msg)
+function chk_led()
 {
-	client = new Paho.MQTT.Client("m12.cloudmqtt.com", 39053,"web_" + parseInt(Math.random() * 100, 10));
-	client.onConnectionLost = onConnectionLost;
-	client.onMessageArrived = onMessageArrived;
-	var options = {
-		useSSL: true,
-		userName: "test",
-		password: "12345",
-		onSuccess:onConnect,
-		onFailure:doFail
-	}
-	client.connect(options);
-	setTimeout(function()
-	{
-		message = new Paho.MQTT.Message(msg);
-		message.destinationName = "/ESP/LED";
-		message.qos = 0;
-		message.retained = true;
-		client.send(message);
-   },5000);
 	
 }
-function onConnect() 
-{
-    // Once a connection has been made, make a subscription and send a message.
-    console.log("onConnect");
-    
- }
-function doFail(e){
-    console.log(e);
-  }
-function onConnectionLost(responseObject)
- {
-    if (responseObject.errorCode !== 0) 
-	{
-      console.log("onConnectionLost:"+responseObject.errorMessage);
-    }
-  }
-function onMessageArrived(message) 
-{
-    console.log("onMessageArrived:"+message.payloadString);
-  }
-
 function onoff(room_num)
 {
 	var x = document.getElementById("room" + room_num).checked;
 	if(x == true)
 	{
 	alert('เปิดไฟห้อง' + room_num);
-	pub_cmd('ON');
+	pub_cmd('ON'+ room_num);
 	}
 	else if (x == false)
 	{
 		alert('ปิดไฟห้อง' + room_num);
-		pub_cmd('OFF');
+		pub_cmd('OFF' + room_num);
 	}
 }
 function pub_cmd(cmd)
@@ -70,9 +30,6 @@ function pub_cmd(cmd)
 			contentType: false,
 			success: function(response) {
                         alert(response);
-                    },
-                    complete: function() {
-                        location.reload();
-                    }				
+                    }			
 			});
 }
